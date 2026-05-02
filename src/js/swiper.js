cdedfeafbc7063799c556abf99596ee9dbb7383f
Kateryna <katyaukr2014@gmail.com>
2026-05-02 18:08:00 +0300

    add

---
+++ src/js/swiper.js
@@ -0,0 +1,32 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+
+// SYSTEMS
+const systemsSwiper = new Swiper('.systems-swiper', {
+  modules: [Navigation],
+  slidesPerView: 2,
+  spaceBetween: 16,
+loop: true,
+  navigation: {
+    nextEl: '.systems-next',
+    },
+    breakpoints: {
+    1440: {
+            slidesPerView: 3,
+        spaceBetween: 24,
+    },
+  },
+});
+
+// GALLERY
+const gallerySwiper = new Swiper('.gallery-swiper', {
+  modules: [Navigation],
+  slidesPerView: 1,
+  spaceBetween: 16,
+
+  navigation: {
+    nextEl: '.gallery-next',
+  },
+});
